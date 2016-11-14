37b3e3f33378663ae530ec3fdd57465351cf9341
Paul Crompton <work@Pauls-MacBook-Pro.local>
2016-11-14 16:24:56 -0500

    Added Settings View Controller

---
+++ Play That Note/SettingsTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsTableViewController.swift
+//  Play That Note
+//
+//  Created by Paul Crompton on 11/14/16.
+//  Copyright © 2016 Paul Crompton. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+    
+    @IBAction func doneButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
