08bf90d12c5b8baab860ecacb14a6d8f80e1438f
Paul Crompton <work@Pauls-MacBook-Pro.local>
2016-11-14 21:52:37 -0500

    Added functionality to Buffer Size Slider in settings

--- Play That Note/SettingsTableViewController.swift
+++ Play That Note/SettingsTableViewController.swift
@@ -13,30 +13,36 @@ class SettingsTableViewController: UITableViewController {
     // MARK: Properties
     @IBOutlet weak var consecutivePitchesLabel: UILabel!
     @IBOutlet weak var consecutivePitchesStepper: UIStepper!
+    @IBOutlet weak var bufferSizeLabel: UILabel!
     @IBOutlet weak var bufferSizeSlider: UISlider!
     @IBOutlet weak var levelThresholdSlider: UISlider!
+    
     
     // MARK: Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         consecutivePitchesStepper.value = Double(Settings.consecutiveMax)
         consecutivePitchesLabel.text = "\(Settings.consecutiveMax) Pitches"
+        bufferSizeLabel.text = "\(Settings.bufferSize)"
+        bufferSizeSlider.value = Float(Settings.bufferSize)
     }
     
     // MARK: IBActions
     @IBAction func doneButton() {
         dismiss(animated: true, completion: nil)
     }
+
     
     @IBAction func consecutivePitchesStepper(_ stepper: UIStepper) {
-        
         consecutivePitchesLabel.text = "\(Int(stepper.value)) Pitches"
         Settings.consecutiveMax = Int(stepper.value)
         print("Stepper Pressed", Settings.consecutiveMax)
     }
     
-    @IBAction func bufferSlider(_ sender: UISlider) {
-        
+    @IBAction func bufferSlider(_ slider: UISlider) {
+        bufferSizeLabel.text = "\(Int(slider.value))"
+        Settings.bufferSize = UInt32(slider.value)
+        print("Slider Value Changed", Settings.bufferSize)
     }
     
     @IBAction func levelThresholdSlider(_ sender: UISlider) {
