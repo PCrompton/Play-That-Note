311b7687f8b897d1f4b729939e57d11aa447cce0
Paul Crompton <work@Pauls-MacBook-Pro.local>
2016-11-18 15:46:01 -0500

    Added stats view controller

---
+++ Play That Note/StatsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  StatsViewController.swift
+//  Play That Note
+//
+//  Created by Paul Crompton on 11/18/16.
+//  Copyright © 2016 Paul Crompton. All rights reserved.
+//
+
+import UIKit
+
+class StatsViewController: CoreDataViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func doneButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
