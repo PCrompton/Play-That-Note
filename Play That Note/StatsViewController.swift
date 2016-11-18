f7365309dc945660a640a4a0c05773b57c4e34c1
Paul Crompton <work@Pauls-MacBook-Pro.local>
2016-11-18 15:53:07 -0500

    Configured table view in stats view controller.

--- Play That Note/StatsViewController.swift
+++ Play That Note/StatsViewController.swift
@@ -8,31 +8,27 @@
 
 import UIKit
 
-class StatsViewController: CoreDataViewController {
+class StatsViewController: CoreDataViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     @IBAction func doneButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StatsCell")
+        return cell!
+    }
 
 }
